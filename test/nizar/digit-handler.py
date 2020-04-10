9cb83f99bdeaa3cf396409121db9b3be447724fa
Nizar-Mhatli <Nizar.Mhatli@fab.pasona.tech>
2020-04-10 18:10:11 +0900

    Started implementing digitformat2coco

---
+++ test/nizar/digit-handler.py
@@ -0,0 +1,150 @@
+from __future__ import annotations
+from typing import List
+import json
+import cv2
+
+from common_utils.check_utils import check_file_exists, check_required_keys, check_dir_exists, check_file_exists
+from common_utils.common_types.bbox import BBox
+from annotation_utils.base.structs import BaseStructObject, BaseStructHandler
+
+from annotation_utils.coco.refactored.structs import COCO_Dataset, \
+    COCO_Info, COCO_License_Handler, COCO_Image_Handler, COCO_Annotation_Handler, COCO_Category_Handler, \
+    COCO_Image, COCO_License, COCO_Annotation, COCO_Category
+
+class DigitBox(BaseStructObject['DigitBox']):
+    def __init__(self, bbox: BBox, label: str):
+        super().__init__()
+        self.bbox = bbox
+        self.label = label
+    
+    def __str__(self) -> str:
+        return f'DigitBox[Label:{self.label}, BBox:{self.bbox}]'
+
+    @classmethod
+    def from_dict(cls, item_dict: dict) -> DigitBox:
+        check_required_keys(
+            item_dict=item_dict,
+            required_keys=[
+                'top', 'left', 'height', 'width', 'label'
+            ]
+        )
+        ymin = item_dict['top']
+        xmin = item_dict['left']
+        bbox_h, bbox_w = item_dict['height'], item_dict['width']
+        bbox = BBox(xmin=xmin, ymin=ymin, xmax=xmin+bbox_w, ymax=ymin+bbox_h)
+        label = item_dict['label']
+        return DigitBox(bbox=bbox, label=label)
+
+    @classmethod
+    def load_from_path(cls, json_path: str) -> DigitBox:
+        check_file_exists(json_path)
+        json_dict = json.load(open(json_path, 'r'))
+        return DigitBox.from_dict(item_dict=json_dict)
+
+class DigitBoxList(BaseStructHandler['DigitBoxList', 'DigitBox']):
+    def __init__(self, digits: List[DigitBox]=None):
+        super().__init__(obj_type=DigitBox, obj_list=digits)
+        self.digits = self.obj_list
+
+    @classmethod
+    def from_dict_list(cls, dict_list: List[dict]) -> DigitBoxList:
+        return DigitBoxList(
+            digits=[DigitBox.from_dict(item_dict) for item_dict in dict_list]
+        )
+
+    @classmethod
+    def load_from_path(cls, json_path: str, strict: bool=True) -> DigitBoxList:
+        check_file_exists(json_path)
+        json_data = json.load(open(json_path, 'r'))
+        return DigitBoxList.from_dict_list(json_data)
+
+class DigitGroup(BaseStructObject['DigitGroup']):
+    def __init__(self, filename: str, digits: DigitBoxList=None):
+        self.filename = filename
+        self.digits = digits if digits is not None else DigitBoxList()
+
+    def __str__(self) -> str:
+        return f'DigitGroup{str(self.__dict__)}'
+
+    @classmethod
+    def from_dict(cls, item_dict: dict) -> DigitGroup:
+        check_required_keys(
+            item_dict=item_dict,
+            required_keys=[
+                'boxes', 'filename'
+            ]
+        )
+        boxes = item_dict['boxes']
+        filename = item_dict['filename']
+        return DigitGroup(filename=filename, digits=DigitBoxList.from_dict_list(boxes))
+
+    @classmethod
+    def load_from_path(cls, json_path: str) -> DigitGroup:
+        check_file_exists(json_path)
+        json_dict = json.load(open(json_path, 'r'))
+        return DigitGroup.from_dict(item_dict=json_dict)
+
+class DigitGroupList(BaseStructHandler['DigitGroupList', 'DigitGroup']):
+    def __init__(self, digits: List[DigitGroup]=None):
+        super().__init__(obj_type=DigitGroup, obj_list=digits)
+        self.groups = self.obj_list
+
+    @classmethod
+    def from_dict_list(cls, dict_list: List[dict]) -> DigitGroupList:
+        return DigitGroupList(
+            digits=[DigitGroup.from_dict(item_dict=item_dict) for item_dict in dict_list]
+        )
+
+    @classmethod
+    def load_from_path(cls, json_path: str, strict: bool=True) -> DigitGroupList:
+        check_file_exists(json_path)
+        json_data = json.load(open(json_path, 'r'))
+        return DigitGroupList.from_dict_list(json_data)
+
+    def to_coco(self, img_dir: str) -> COCO_Dataset:
+        check_dir_exists(img_dir)
+
+        # Initialize Handlers
+        info = COCO_Info(description='COCO Dataset Converted From Digits Format')
+        licenses = COCO_License_Handler()
+        images = COCO_Image_Handler()
+        annotations = COCO_Annotation_Handler()
+        categories = COCO_Category_Handler()
+
+        # Load Up The Handlers
+        licenses.append(
+            COCO_License(url='N/A', id=0, name='Free License')
+        )
+
+        for group in self:
+            img_filename = group.filename
+            img_path = f'{img_dir}/{img_filename}'
+            check_file_exists(img_path)
+            img = cv2.imread(img_path)
+            img_h, img_w = img.shape[:2]
+            coco_image = COCO_Image.from_img_path(
+                img_path=img_path,
+                license_id=0,
+                image_id=len(images)
+            )
+            images.append(coco_image)
+            for digit in group.digits:
+                bbox = digit.bbox
+                label = digit.label
+                raise NotImplementedError # TODO
+
+        # Construct COCO Dataset
+        dataset = COCO_Dataset(
+            info=info,
+            licenses=licenses,
+            images=images,
+            annotations=annotations,
+            categories=categories
+        )
+        return dataset
+
+from logger import logger
+ann_path = 'data/digitStruct.json'
+img_dir = 'data/images'
+digit_groups = DigitGroupList.load_from_path(ann_path)
+logger.purple(digit_groups)
